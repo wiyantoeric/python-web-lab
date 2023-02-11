060d4f051efc07cc519ddc43d9a341662f0635a9
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-02-11 22:27:17 +0700

    update line chart

--- streamlit-line-chart/app.py
+++ streamlit-line-chart/app.py
@@ -2,17 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(
+    page_title='Line Chart',
+    page_icon=None,
+)
+
 # Create chart with random value
 data_count = 100
-random_data = np.array([i*0.1+np.random.randn() for i in range (data_count)])
+random_data = np.array([i*0.1 + np.random.randn() for i in range (data_count)])
 
 chart_data = pd.DataFrame(
         random_data,
-        columns=['Value'],
+        columns=['Random Value'],
         dtype=float,
     )
 
 # Render UI
-st.header('Random Data Graph')
-st.caption('Python web lecture assignment')
+st.header('Line Chart')
+st.caption('Random data graph')
 st.line_chart(chart_data)
